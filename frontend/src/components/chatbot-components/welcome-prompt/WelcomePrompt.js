bdc40ae1f579eb4638a3b4413eeaf4694f155c8d
Shubham <shubhamsinghmys@gmail.com>
2024-12-08 21:43:15 +0530

    changed welcome prompt

--- frontend/src/components/chatbot-components/welcome-prompt/WelcomePrompt.js
+++ frontend/src/components/chatbot-components/welcome-prompt/WelcomePrompt.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import {React, useContext} from 'react';
 import './WelcomePrompt.css';
+import ChatbotContext from '../../../context/chatbotContext/ChatbotContext';
 
 export default function WelcomePrompt() {
+
+  const {conversationsInfo, currentConversationID} = useContext(ChatbotContext);
   return (
     <div className='welcome-prompt'>
-      Create Or Continue A Conversation. Let's get coding!
+      {
+        (conversationsInfo.length === 0)?
+          "Create A New Conversation & Let's Get Coding!" 
+          :
+          (currentConversationID === null)?
+            "Create Or Continue A Conversation & Let's Get Coding!"
+            :
+            ""
+      }
     </div>
   )
 }
